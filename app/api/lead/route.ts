d61005ce308f324c0e9ab98a6680a38ef9bf7604
firstklasindustries-jpg <firstklasindustries@gmail.com>
2025-12-11 11:48:29 +0100

    Update route.ts

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -1,6 +1,6 @@
 // app/api/lead/route.ts
 import { NextResponse } from "next/server";
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 export async function POST(req: Request) {
   try {
