1fcddd29808402b0e2e41360316db0029fa6dcb0
firstklasindustries-jpg <firstklasindustries@gmail.com>
2025-12-11 11:26:14 +0100

    Delete lib/supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabaseAdmin.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
-// OBS: SUPABASE_SERVICE_ROLE_KEY måste vara satt i Vercel env
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
